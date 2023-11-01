5411d17c95463129f6872ea38d5b3cca872fc9d1
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-01 18:27:12 +0500

    Search in One Database function

--- University API/Services/StudentService.cs
+++ University API/Services/StudentService.cs
@@ -20,5 +20,38 @@ namespace University_API.Services
                 return student;
             }
         }
+
+        public List<string> OneDatabase(string task)
+        {
+            using(SqlConnection connection=new SqlConnection(connectionString))
+            {
+                var tableName = connection.Query<string>("Select Table_Name from Information_Schema.Tables",CommandType.StoredProcedure).ToList();
+                var result=new List<string>();
+                foreach (var table in tableName)
+                {
+                    string query = $"Select Column_Name from Information_Scheme.Columns where Table_Name='{table}";
+                    var res = connection.Query<string>(query).ToList();
+                    foreach(var i in res)
+                    {
+                        string sql = $"Select * from {table} where {i}='{task}'";
+                        try
+                        {
+                            var checkresult = connection.Query<string>(sql).ToList();
+                            if(checkresult.Count!=0)
+                            {
+                                result.Add($"{task}");
+                            }
+                            
+                        }
+                        catch(Exception e)
+                        {
+
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+       
     }
 }
